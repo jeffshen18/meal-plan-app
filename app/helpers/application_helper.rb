8fa31986ef9d66370ce54f4256d31c0fe769b45b
jeffshen18 <jeffreyshen18@gmail.com>
2018-10-07 17:38:21 -0600

    adding styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+
 module ApplicationHelper
+  def alert_class(flash_type)
+    case flash_type.to_sym
+    when :notice
+      "alert-success"
+    when :alert
+      "alert-warning"
+    when :error
+      "alert-danger"
+    end
+  end
 end
